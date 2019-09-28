2ab37bbd384a784051411aec7bb4a27fb5a4d33f
Natnael <nwondwes@ryerson.ca>
2019-09-28 11:49:56 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var expressHbs = require('express-handlebars');
+
+//var _server = new MongoClient(connectionString).GetServer();
+//var connectionString = "mongodb+srv://brad123:<brad123>@cluster0-zjpoc.mongodb.net/test?retryWrites=true&w=majority";
+//var _databaseName = MongoUrl.Create(connectionString).DatabaseName;
+//var _server = MongoServer.Create(connectionString);
+//_server.GetDatabase(_databaseName);
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+
+// view engine setup
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
